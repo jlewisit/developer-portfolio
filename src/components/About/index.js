7eea4a15df510253cde5c7f620793c2309009e0f
jlewisit <joelewis.mortgage@gmail.com>
2022-11-13 03:31:54 -0800

    About complete

---
+++ src/components/About/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+          <p>
+            I'm a full stack developer with strengths in communication, team
+            work, and beating deadlines. I recently earned a certificate in Full
+            Stack Web Development from the University of Texas at Austin, where
+            I developed skills in JavaScript, Node.js, React.js, and more.
+          </p>
+          <p>
+            An innovative problem-solver passionate about developing apps, my
+            current focus is on mobile-first design and development. Since
+            completing my full stack development program, I have continued my
+            studies independently to learn more about Android and WordPress
+            development. When I'm not designing or developing, I enjoy hiking,
+            cycling, or reading up on the latest trends in web and mobile
+            development. I’m excited to leverage my skills as part of a
+            fast-paced, quality-driven team. Reach out to me to
+            <a href="#contact"><span class="p-a">chat</span></a> about our next
+            project <a href="#contact"><span class="p-a">here</span></a
+            >.
+          </p>
+        </div>
+    );
+}
+
+export default About;
